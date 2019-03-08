6ecc37934743bd93bc095a67f52e37cb6316d02d
riane2 <nuaa1612104@163.com>
2019-03-08 16:56:23 +0800

    beanfactory

---
+++ src/main/java/com/spring/ioc/beanfactory/ApplicationContextDemo.java
@@ -0,0 +1,12 @@
+package com.spring.ioc.beanfactory;
+
+import org.springframework.context.ApplicationContext;
+
+/**
+ * {@linkplain ApplicationContext}:应用上下文，建立在BeanFactory基础之上，提供更多面向应用的功能
+ * <p>
+ * BeanFactory时Spring框架的基础设施，面向Spring本身
+ * ApplicationContext面向使用Spring框架的开发者，几乎所有的应用场合都可以直接使用ApplicationContext而非BeanFactory
+ */
+public class ApplicationContextDemo {
+}
---
+++ src/main/java/com/spring/ioc/beanfactory/BeanFactoryDemo.java
@@ -0,0 +1,9 @@
+package com.spring.ioc.beanfactory;
+
+import org.springframework.beans.factory.BeanFactory;
+
+/**
+ * {@linkplain BeanFactory}:Spring IOC 容器
+ */
+public class BeanFactoryDemo {
+}
